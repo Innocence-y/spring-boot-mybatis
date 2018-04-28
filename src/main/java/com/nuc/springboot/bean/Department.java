ff877605a5f26eb089c64fe35bb0dbd32c5aa970
Innocence-y <2274747912@qq.com>
2018-04-28 22:50:52 +0800

    Initial commit

---
+++ src/main/java/com/nuc/springboot/bean/Department.java
@@ -0,0 +1,15 @@
+package com.nuc.springboot.bean;
+
+/**
+ * 描述:部门
+ *
+ * @author 闫文强
+ * @create 2018-04-28 22:24
+ */
+
+public class Department {
+
+	private Integer id;
+	private String departmentName;
+
+}
